db4ce5f1d43543b98f024fe1cc0b03d0a78e8612
Sourav9160 <souravbhowmik2010@gmail.com>
2026-05-17 21:35:27 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import { DexView } from '@/components/DexView';
+
+export default function HomePage() {
+  return (
+    <main className="pt-24 pb-32 px-margin-mobile md:px-margin-desktop min-h-screen max-w-container-max mx-auto">
+      <DexView />
+    </main>
+  );
+}
